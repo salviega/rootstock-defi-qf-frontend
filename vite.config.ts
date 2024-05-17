669c1b3b0cd40c3f9841abc30033b58bc1dec9b3
Santiago Viana <salviega6@gmail.com>
2024-05-17 17:14:38 -0500

    :heavy_plus_sign: Eslint plugins added

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import path from 'path'
-import { defineConfig } from 'vite'
+import path from 'path';
+import { defineConfig } from 'vite';
 
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src')
-		}
-	}
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
