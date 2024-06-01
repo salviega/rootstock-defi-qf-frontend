d2e4d52d772436d8899fd02b5ee557ad43353243
Joseth Calderon <stevencorredor04@gmail.com>
2024-06-01 00:13:20 -0500

    💄 Nav component was added in dashboard

--- src/components/dashboard/Aside.tsx
+++ src/components/dashboard/Aside.tsx
@@ -6,7 +6,7 @@ import Connection from "./Aside/Connection"
 export default function Aside(): JSX.Element {
   
   return(
-    <aside className="flex flex-col items-center justify-center w-[32%] h-full gap-6 bg-secdcolor py-4 rounded-lg">
+    <aside className="aside__container flex flex-col items-center justify-center w-[32%] h-full gap-6 bg-secdcolor py-4 rounded-lg">
       <Logo />
       <Views />
       <Projects />
